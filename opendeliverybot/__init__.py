e21787afd3265b484fcc1eb874163456a0af46bd
SilkePilon <silkepilon2009@gmail.com>
2023-08-21 21:27:17 +0200

    recode to remove the config and use Click

--- opendeliverybot/__init__.py
+++ opendeliverybot/__init__.py
@@ -7,11 +7,11 @@ import time
 import contextlib
 import io
 import sys
+import click
 import json
 import os
 from tqdm import tqdm
 from datetime import datetime
-import matplotlib.pyplot as plt
 
 
 def clear():
@@ -31,8 +31,8 @@ def nostdout():
 
 
 # Initialize mineflayer bot and plugins
-print("installing javascript libaries...")
 pbar = tqdm(total=8)
+pbar.set_description("installing javascript libaries", refresh=True)
 with nostdout():
   mcData = require('minecraft-data')
 pbar.update(1)
@@ -62,116 +62,241 @@ pbar.close()
 clear()
 
 
-config_defualt = {
-    "Username": "OpenDeliveryBot",
-    "Password": "<PASSWORD>",
-    "Host": "127.0.0.1",
-    "Port": 25565,
-    "Auth": "microsoft",
-    "Version": "auto",
-    "CheckTimeoutInterval": 600000,
-    "ViewerPort": 8000,
-    "Goto": ["100", "100", "100"],
-    "ChestRage": 100,
-    "InitChestType": "chest",
-    "InitChestCords": ["100", "100", "100"],
-    "InitItemsName": "SchulkerBox",
-    "InitItemsCount": 1,
-    "ClientUsername": "OpenDeliveryBot"
-}
 
-
-config_defualt = json.dumps(config_defualt, indent=4)
-
-
-if os.path.isfile("config.json") == False:
-  with open('config.json', 'w') as g:
-    g.write(config_defualt)
-
+# Function for implementing the loading animation
+def load_animation(text):
+  
+    # String to be displayed when the application is loading
+    load_str = text
+    ls_len = len(load_str)
+  
+  
+    # String for creating the rotating line
+    animation = "|/-\\"
+    anicount = 0
+      
+    # used to keep the track of
+    # the duration of animation
+    counttime = 0        
+      
+    # pointer for travelling the loading string
+    i = 0                     
+  
+    while (counttime != 100):
+          
+        # used to change the animation speed
+        # smaller the value, faster will be the animation
+        time.sleep(0.075) 
+                              
+        # converting the string to list
+        # as string is immutable
+        load_str_list = list(load_str) 
+          
+        # x->obtaining the ASCII code
+        x = ord(load_str_list[i])
+          
+        # y->for storing altered ASCII code
+        y = 0                             
+  
+        # if the character is "." or " ", keep it unaltered
+        # switch uppercase to lowercase and vice-versa 
+        if x != 32 and x != 46:             
+            if x>90:
+                y = x-32
+            else:
+                y = x + 32
+            load_str_list[i]= chr(y)
+          
+        # for storing the resultant string
+        res =''             
+        for j in range(ls_len):
+            res = res + load_str_list[j]
+              
+        # displaying the resultant string
+        sys.stdout.write("\r"+res + animation[anicount])
+        sys.stdout.flush()
+  
+        # Assigning loading string
+        # to the resultant string
+        load_str = res
+  
+          
+        anicount = (anicount + 1)% 4
+        i =(i + 1)% ls_len
+        counttime = counttime + 1
+      
+    # for windows OS
+    clear()
+    
+load_animation("starting open delivery bot... ")
 
 # Load bot config from JSON file
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-# Constants
-RANGE_GOAL = -1  
-CLIENT_USERNAME = config['ClientUsername']
-DEV = False
 
-# Create version
-if config['Version'] == "auto":
-  version = False
-else:
-  version = config['Version']
+@click.command()
+@click.option("--username", help="Username for login.")
+@click.option("--password", help="Password for login.")  
+@click.option("--host", prompt=True, help="Hostname or IP address of server.")
+@click.option("--port", default=25565, help="Port number of server.")
+@click.option("--auth", default="microsoft", help="Authentication method.",prompt=True)
+@click.option("--version", default="auto", help="Game version.",prompt=True)
+@click.option("--check_timeout", default=600000, help="Timeout interval for checks.")
+@click.option("--viewer_port", default=8000, help="Port for viewer.")
+@click.option("--goto", default=["100", "100", "100"], multiple=True, help="Coordinates to go to.", prompt=True)
+@click.option("--chest_range", default=100, help="Range to search for chests.")
+@click.option("--init_chest_type", default="chest", help="Type of chest to look for when starting.")
+@click.option("--init_chest_cords", default=["100", "100", "100"], multiple=True, help="Coordinates to base chest.", prompt=True)  
+@click.option("--init_items_name", default="SchulkerBox", help="Name of items to get from the base chest.", prompt=True)
+@click.option("--init_items_count", default=1, help="Number of items to get from the base chest.",prompt=True)
+@click.option("--client_username", default="OpenDeliveryBot", help="Username to deliver to.", prompt=True)
+def main(username, password, host, port, auth, version, check_timeout, viewer_port, goto, chest_range, init_chest_type, init_chest_cords, init_items_name, init_items_count, client_username):
 
-# Create bot instance 
-bot = mineflayer.createBot({
-  'host': config['Host'],
-  'port': config['Port'],
-  'username': config['Username'],
-  'password': config['Password'],
-  'auth': config['Auth'],
-  'hideErrors': True,
-  'version': version,
-  'checkTimeoutInterval': config['CheckTimeoutInterval'],
-})
+  DEV = False
+  # Create version
+  if config['Version'] == "auto":
+    version = False
+  else:
+    version = config['Version']
 
-# Load plugins
-bot.loadPlugin(pathfinder.pathfinder) 
-bot.loadPlugin(elytrafly.elytrafly)
-bot.loadPlugin(taskManager)
-print('Started Open Delivery Bot')
-  
-# Login handler  
-@On(bot, 'login')
-def handle_login(*args):
-  print(f'joined {config["Host"]}:{config["Port"]}')
-  print(bot.entity.position)
-  mineflayerViewer(bot, { "port": config['ViewerPort'] })
-  global mcData
-  mcData = require('minecraft-data')(bot.version)
-  movements = pathfinder.Movements(bot, mcData)
-  
-  with open("playes.log", "w") as x:
-    x.write(str(bot.players))
-  
-  with open("cords.log", "a") as x:
-    x.write(f"{datetime.now()}  |  {config['Host']}:{config['Port']}  |  X:{str(bot.entity.position.x)}, Y:{str(bot.entity.position.y)}, Z:{str(bot.entity.position.z)}")
-  
-  # Disable digging if needed
-  movements.canDig = False
+  # Create bot instance 
+  bot = mineflayer.createBot({
+    'host': host,
+    'port': port,
+    'username': username,
+    'password': password,
+    'auth': auth,
+    'hideErrors': True,
+    'version': version,
+    'checkTimeoutInterval': check_timeout,
+  })
 
-  bot.pathfinder.setMovements(movements)
-  
-  # Main login logic
-  if DEV == True:
-    bot.taskManager.Add("Get Items From Chest", GetItems(config['InitItemsName'], config['InitItemsCount']), 500)
-    bot.taskManager.Add("Go To Specified Location", GoToLocation(config['goto']), 500)
-    bot.taskManager.Add("Deposit Items To Nearby Chest", DepositItems(""), 500)
-    bot.taskManager.Add("Self-destruct And Respawn", Respawn(), 500)
+  # Load plugins
+  bot.loadPlugin(pathfinder.pathfinder) 
+  bot.loadPlugin(elytrafly.elytrafly)
+  bot.loadPlugin(taskManager)
+  print('Started Open Delivery Bot')
     
-    print(", ".join([e.name for e in bot.taskManager.GetWholeQueue()]))
-  
-
-
-
-
-
-# Function to find and open the init chest
-def GetItems(items, count):
-  
-  if config['InitChestCords'] == ["0", "0", "0"]:
+  # Login handler  
+  @On(bot, 'login')
+  def handle_login(*args):
+    print(f'joined {host}:{port}')
+    print(bot.entity.position)
+    mineflayerViewer(bot, { "port": viewer_port })
+    global mcData
+    mcData = require('minecraft-data')(bot.version)
+    movements = pathfinder.Movements(bot, mcData)
     
+    with open("playes.log", "w") as x:
+      x.write(str(bot.players))
+    
+    with open("cords.log", "a") as x:
+      x.write(f"{datetime.now()}  |  {host}:{port}  |  X:{str(bot.entity.position.x)}, Y:{str(bot.entity.position.y)}, Z:{str(bot.entity.position.z)}")
+    
+    # Disable digging if needed
+    movements.canDig = False
+
+    bot.pathfinder.setMovements(movements)
+    
+    # Main login logic
+    if DEV == True:
+      bot.taskManager.Add("Get Items From Chest", GetItems(init_items_name, init_items_count), 500)
+      bot.taskManager.Add("Go To Specified Location", GoToLocation(goto), 500)
+      bot.taskManager.Add("Deposit Items To Nearby Chest", DepositItems(""), 500)
+      bot.taskManager.Add("Self-destruct And Respawn", Respawn(), 500)
+      
+      print(", ".join([e.name for e in bot.taskManager.GetWholeQueue()]))
+    
+
+
+
+
+
+  # Function to find and open the init chest
+  def GetItems(items, count):
+    
+    if init_chest_cords == ["0", "0", "0"]:
+      
+
+      foundChest = False
+      
+      while not foundChest:
+          
+        # Find nearby chest
+        
+        chestToOpen = bot.findBlock({
+            'matching': [mcData.blocksByName[name].id for name in [f'{str(init_chest_type).lower()}']],
+            'maxDistance': 100,
+        })
+        
+        if not chestToOpen and foundChest == False:
+          bot.chat('no chest found')
+          continue
+          
+        # Go to chest location  
+        if chestToOpen.position.x:
+          x = chestToOpen.position.x
+          y = chestToOpen.position.y  
+          z = chestToOpen.position.z
+
+          bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Found a nearby chest to deliver: [{str(x)}, {str(y)}, {str(z)}]')
+          locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
+          
+          try:
+            chest = bot.openContainer(chestToOpen)  
+          except Exception:    
+            bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Please place a new chest in my location.')
+            continue
+              
+          print(chest.containerItems())
+          time.sleep(10)
+          chest.close()
+          bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Items have been successfully delivered at: [{str(x)}, {str(y)}, {str(z)}] on [{datetime.now()}]')
+          foundChest = True
+          break
+    else:
+      x = init_chest_cords[0]
+      y = init_chest_cords[1]
+      z = init_chest_cords[2]
+      locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
+
+
+
+
+
+
+  # Function to go to cordinates
+  def GoToLocation(cordinates):
+      x = cordinates[0]
+      y = cordinates[1]
+      z = cordinates[2]
+      bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Going to: [{str(x)}, {str(y)}, {str(z)}]')
+      locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
+      
+
+
+
+
+
+
+
+
+
+
+
+
+  # Function to find and open chest  
+  def DepositItems(items):
 
     foundChest = False
     
     while not foundChest:
         
       # Find nearby chest
-      
       chestToOpen = bot.findBlock({
-          'matching': [mcData.blocksByName[name].id for name in [f'{str(config["InitChestType"]).lower()}']],
-          'maxDistance': 100,
+          'matching': [mcData.blocksByName[name].id for name in ['chest']], 
+          'maxDistance': chest_range,
       })
       
       if not chestToOpen and foundChest == False:
@@ -184,120 +309,55 @@ def GetItems(items, count):
         y = chestToOpen.position.y  
         z = chestToOpen.position.z
 
-        bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Found a nearby chest to deliver: [{str(x)}, {str(y)}, {str(z)}]')
+        bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Found a nearby chest to deliver: [{str(x)}, {str(y)}, {str(z)}]')
         locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
         
         try:
           chest = bot.openContainer(chestToOpen)  
         except Exception:    
-          bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Please place a new chest in my location.')
+          bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Please place a new chest in my location.')
           continue
             
         print(chest.containerItems())
         time.sleep(10)
         chest.close()
-        bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Items have been successfully delivered at: [{str(x)}, {str(y)}, {str(z)}] on [{datetime.now()}]')
+        bot.chat(f'/tell {client_username} [OPEN DELIVERY BOT] Items have been successfully delivered at: [{str(x)}, {str(y)}, {str(z)}] on [{datetime.now()}]')
         foundChest = True
         break
-  else:
-    x = config['InitChestCords'][0]
-    y = config['InitChestCords'][1]
-    z = config['InitChestCords'][2]
-    locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
+        
 
+  # Function to find lava and selfdiscructs
+  def Respawn():
 
-
-
-
-
-# Function to go to cordinates
-def GoToLocation(cordinates):
-    x = cordinates[0]
-    y = cordinates[1]
-    z = cordinates[2]
-    bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Going to: [{str(x)}, {str(y)}, {str(z)}]')
-    locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
+    foundLava = False
     
-
-
-
-
-
-
-
-
-
-
-
-
-# Function to find and open chest  
-def DepositItems(items):
-
-  foundChest = False
-  
-  while not foundChest:
+    while not foundLava:
+        
+      # Find nearby lava
+      LavaToFind = bot.findBlock({
+          'matching': [mcData.blocksByName[name].id for name in ['lava']], 
+          'maxDistance': 200,
+      })
       
-    # Find nearby chest
-    chestToOpen = bot.findBlock({
-        'matching': [mcData.blocksByName[name].id for name in ['chest']], 
-        'maxDistance': config['ChestRage'],
-    })
-    
-    if not chestToOpen and foundChest == False:
-      bot.chat('no chest found')
-      continue
-      
-    # Go to chest location  
-    if chestToOpen.position.x:
-      x = chestToOpen.position.x
-      y = chestToOpen.position.y  
-      z = chestToOpen.position.z
-
-      bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Found a nearby chest to deliver: [{str(x)}, {str(y)}, {str(z)}]')
-      locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
-      
-      try:
-        chest = bot.openContainer(chestToOpen)  
-      except Exception:    
-        bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Please place a new chest in my location.')
+      if not LavaToFind and foundLava == False:
+        bot.chat('no chest found')
         continue
-          
-      print(chest.containerItems())
-      time.sleep(10)
-      chest.close()
-      bot.chat(f'/tell {CLIENT_USERNAME} [OPEN DELIVERY BOT] Items have been successfully delivered at: [{str(x)}, {str(y)}, {str(z)}] on [{datetime.now()}]')
-      foundChest = True
-      break
-      
-
-# Function to find lava and selfdiscructs
-def Respawn():
-
-  foundLava = False
-  
-  while not foundLava:
-      
-    # Find nearby lava
-    LavaToFind = bot.findBlock({
-        'matching': [mcData.blocksByName[name].id for name in ['lava']], 
-        'maxDistance': 200,
-    })
-    
-    if not LavaToFind and foundLava == False:
-      bot.chat('no chest found')
-      continue
-      
-    # Go to lava location  
-    if LavaToFind.position.x:
-      x = LavaToFind.position.x
-      y = LavaToFind.position.y  
-      z = LavaToFind.position.z
-      locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
-      break
+        
+      # Go to lava location  
+      if LavaToFind.position.x:
+        x = LavaToFind.position.x
+        y = LavaToFind.position.y  
+        z = LavaToFind.position.z
+        locaton = bot.pathfinder.goto(pathfinder.goals.GoalNear(x, y, z, 1), timeout=60)
+        break
 
 
 
-# Handler for bot end  
-@On(bot, 'end')
-def handle_end(*args):
-  print('Bot ended!', args)
+  # Handler for bot end  
+  @On(bot, 'end')
+  def handle_end(*args):
+    print('Bot ended!', args)
+
+
+#run main
+main()
