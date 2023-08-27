af44fecc8405f7aa8bbbd2eacd009d075ce4506b
SilkePilon <silkepilon2009@gmail.com>
2023-08-27 12:44:56 +0200

    test

--- opendeliverybot/__main__.py
+++ opendeliverybot/__main__.py
@@ -2,14 +2,22 @@ import os
 import sys
 from .console_ui import consoleui
 
-mode = 1
-try:
-    if sys.stdin.isatty():
-        mode = 0
-except AttributeError:  # stdin is NoneType if not in terminal mode
-    pass
+is_linux = sys.platform.startswith('linux')
+is_mac = sys.platform == 'darwin'
+is_windows = os.environ.get('windir') 
 
-if mode == 0:
+if is_linux or is_mac:
+  if os.environ.get('DISPLAY','') == '':
+    print('No GUI on Linux/Mac, running console UI')  
     consoleui()
-else:
+  else:
+    print('Linux/Mac GUI found, running Streamlit')
     os.system("streamlit run web.py")
+
+elif is_windows:
+  print('Windows found, running Streamlit')
+  os.system("streamlit run web.py") 
+
+else:
+  print('Unknown platform, defaulting to console UI')
+  consoleui()
