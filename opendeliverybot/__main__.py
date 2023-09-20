dfea8ef58bd6ecd9874ecfdae9e3836e7a509f77
Silke pilon <silkepilon2009@gmail.com>
2023-09-20 11:05:41 +0000

    fix small bug in __main__.py

--- opendeliverybot/__main__.py
+++ opendeliverybot/__main__.py
@@ -3,7 +3,7 @@ import sys
 import click
 
 @click.command(context_settings={"ignore_unknown_options": True})
-@click.option("--console", default=False, is_flag=True, help="Use console UI instead of GUI")
+@click.option("--console", default=False, is_flag=True, help="Force the app to use the console")
 @click.argument('args', nargs=-1)
 def run(console, args):
 
@@ -14,12 +14,12 @@ def run(console, args):
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
     if console:
-        os.system(f"{sys.argv[0]} {script_directory}/console_ui.py {' '.join(args)}")
+        os.system(f"python {script_directory}/console_ui.py {' '.join(args)}")
     else:
         if is_linux or is_mac:
             if os.environ.get('DISPLAY') == '':
                 print('No GUI, using console UI')
-                os.system(f"{sys.argv[0]} {script_directory}/console_ui.py {' '.join(args)}")
+                os.system(f"python {script_directory}/console_ui.py {' '.join(args)}")
             else:
                 print('Using GUI')
                 print('Web ui is still in developmet and is not working atm, rerun the command again with --console')
@@ -28,7 +28,7 @@ def run(console, args):
             print('Web ui is still in developmet and is not working atm, rerun the command again with --console')
         else:
             print('Unknown platform, using console UI')
-            os.system(f"{sys.argv[0]} {script_directory}/console_ui.py {' '.join(args)}")
+            os.system(f"python {script_directory}/console_ui.py {' '.join(args)}")
 
 if __name__ == "__main__":
     run()
