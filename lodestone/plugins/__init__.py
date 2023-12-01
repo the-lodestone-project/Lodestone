d3c69f0458ef711fb0d97f3ff56053e261cc4274
Silke pilon <silkepilon2009@gmail.com>
2023-12-01 22:12:36 +0100

    Update plugins subproject commit hash

---
+++ lodestone/plugins/__init__.py
@@ -0,0 +1,12 @@
+from cactus_farm_builder import cactus
+from discord_webhook import discord
+from schematic_builder import schematic
+from discordrp import discordrp
+
+class plugins:
+    def __init__(self):
+        self.cactus = cactus()
+        self.discordrp = discordrp()
+        self.discord = discord()
+        self.schematic = schematic()
+        super().__init__()
