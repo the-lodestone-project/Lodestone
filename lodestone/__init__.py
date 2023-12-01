df99eee0e2c9aa6f7fd5daca50215c6a6b178ab8
Silke pilon <silkepilon2009@gmail.com>
2023-12-01 21:45:23 +0100

    Refactor import statements in __init__.py

--- lodestone/__init__.py
+++ lodestone/__init__.py
@@ -1,22 +1,4 @@
 import os
-try:
-    from lodestone.bot import createBot, Bot
-    from lodestone.utils import llm
-    from lodestone.api import fastapi
-    from lodestone.server import createServer, Server
-    from javascript import On as Event
-    
-    from lodestone.logger import logger
-    from lodestone.gradios import ui
-except:
-    from .bot import createBot, Bot
-    from .utils import llm
-    from .api import fastapi
-    from .server import createServer, Server
-    from javascript import On as Event
-    from .logger import logger
-    from .gradios import ui
-    
 
 if os.path.isdir('plugins'):
     try:
@@ -37,3 +19,21 @@ else:
         from lodestone.plugins import Base as plugins
     except:
         from .plugins import Base as plugins
+
+try:
+    from lodestone.bot import createBot, Bot
+    from lodestone.utils import llm
+    from lodestone.api import fastapi
+    from lodestone.server import createServer, Server
+    from javascript import On as Event
+    from lodestone.logger import logger
+    from lodestone.gradios import ui
+except:
+    from .bot import createBot, Bot
+    from .utils import llm
+    from .api import fastapi
+    from .server import createServer, Server
+    from javascript import On as Event
+    from .logger import logger
+    from .gradios import ui
+    
