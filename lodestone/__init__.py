5abc2d525dec950d68b4819ba6228e0416f3747a
Silke pilon <silkepilon2009@gmail.com>
2023-12-02 19:20:08 +0100

    Update version in pyproject.toml and remove unused plugin files

--- lodestone/__init__.py
+++ lodestone/__init__.py
@@ -1,14 +1,14 @@
 try:
     from lodestone.bot import createBot, Bot
     from lodestone.utils import llm
-    from plugins import plugins
+    from plugin import plugins
     from lodestone.api import fastapi
     from lodestone.server import createServer, Server
     from lodestone.logger import logger
 except:
     from .bot import createBot, Bot
     from .utils import llm
-    from .plugins import plugins
+    from .plugin import plugins
     from .api import fastapi
     from .server import createServer, Server
     from .logger import logger
