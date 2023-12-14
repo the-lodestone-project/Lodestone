6d2a547901edde28816af57dd9d787e63012678c
Silke pilon <silkepilon2009@gmail.com>
2023-12-14 18:44:42 +0100

    Update lodestone package version and fix imports

--- lodestone/__init__.py
+++ lodestone/__init__.py
@@ -1,13 +1,13 @@
 try:
     from lodestone.bot import createBot, Bot
     from lodestone.utils.utils import llm
-    from lodestone.modules.api.api import fastapi
+    from lodestone.modules.api import fastapi
     from lodestone.server import createServer, Server
-    from lodestone.modules.logger.logger import logger
+    from lodestone.modules.logger import logger
 except:
     from .bot import createBot, Bot
     from .utils.utils import llm
-    from .modules.api.api import fastapi
+    from .modules.api import fastapi
     from .server import createServer, Server
-    from .modules.logger.logger import logger
+    from .modules.logger import logger
     
--- lodestone/__main__.py
+++ lodestone/__main__.py
@@ -1,120 +1,41 @@
-import os
-import sys
+
 import click
-from lodestone import fastapi
-import subprocess
-import time
 from rich.console import Console
 import lodestone
 from dotenv import load_dotenv
-import requests
+import threading
+import lodestone
 
-@click.command(context_settings={"ignore_unknown_options": True})
-@click.option("--console", "-c", default=False, is_flag=True, help="Force the app to use the console")
-@click.option("--test", "-t", is_flag=True, help="Run version tests on Lodestone")
-@click.option("--host", "-h", help="Host to run Lodestone's gui on", default=None)
-@click.option("--port", "-p", help="Port to run Lodestone's gui on", default=8000)
-@click.argument('args', nargs=-1)
-def run(console, test, host, port, args):
+@click.command()
+@click.option("--username", "-u", help="Run lodestone tests", required=True, type=str)
+@click.option("--host", "-h", help="Run lodestone tests", required=True, type=str, show_default=True)
+@click.option("--port", "-p", default=25565, help="Run lodestone tests", required=False, type=int, show_default=True)
+@click.option("--version", "-v", default="auto", help="Run lodestone tests", required=False, show_default=True, type=click.Choice(["1.8.8", "1.9" "15w40b", "1.9.1-pre2", "1.9.2", "1.9.4", "1.10", "16w20a", "1.10-pre1", "1.10", "1.10.1", "1.10.2", "1.11", "16w35a", "1.11", "1.11.2", "1.12", "17w15a", "17w18b", "1.12-pre4", "1.12", "1.12.1", "1.12.2", "1.13", "17w50a", "1.13", "1.13.1", "1.13.2-pre1", "1.13.2-pre2", "1.13.2", "1.14", "1.14", "1.14.1", "1.14.3", "1.14.4", "1.15", "1.15", "1.15.1", "1.15.2", "1.16", "20w13b", "20w14a", "1.16-rc1", "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.17", "21w07a", "1.17", "1.17.1", "1.18", "1.18", "1.18.1", "1.18.2", "1.19", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1"], case_sensitive=False))
+def run(username, host, port, version):
+    def bot_process():
+        bot = lodestone.createBot(
+            host=host,
+            port=port,
+            username=username,
+            version=version,
+            ls_enable_chat_logging=False,
+        )
+    def input_process():
+        while True:
+            commad = input("lodestone > ")
+            if "bot" in globals():
+                print(commad)
+            else:
+                print("lodestone is not running")
     
-    if test:
-        versions = ["auto", "1.20.1", "1.19", "1.18", "1.17", "1.16", "1.15", "1.14", "1.13", "1.11", "1.10", "1.9", "1.8"]
-
-        console = Console()
-
-        with console.status("[bold]Checking versions...\n") as status:
-            for version in versions:
-                failed = ""
-                status.update(f"Checking version {version}")
-
-                # Create bot with the current version  
-                try:
-                    bot = lodestone.createBot(
-                        host='2b2t.org',
-                        username='TestBot',  
-                        version=version,
-                        checkTimeoutInterval=70,
-                        ls_skip_checks=True,
-                        hideErrors=False,
-                        ls_debug_mode=True,
-                        ls_disable_viewer=True
-                    )
-                except:
-                    failed = "failed ([bold red]❌[reset])"
-                    print(f"Version {version} failed to connect")
-                    
+    bot_thread = threading.Thread(target=bot_process)
+    bot_thread.start()
+    input_thread = threading.Thread(target=input_process)
+    input_thread.start()
                 
-                # Check connection
-                if bot.tablist:
-                    failed = "passed ([bold green]✔️[reset])"
-                    status.update(f"Checking version {version} {failed}")
-                else:
-                    failed = "failed ([bold red]❌[reset])"
-                    print(f"Version {version} failed to connect")
-                
-                # Stop bot
-                bot.stop()
-
-                # Add delay to avoid congestion
-                for i in range(0, 10):
-                    
-                    status.update(f"{version} {failed} ({10 - i} until next version)")
-                    time.sleep(1)
-    
-    def check_python_command():
-        try:
-            subprocess.check_output(['python', '--version'])
-            return 'python'
-        except:
-            try:
-                subprocess.check_output(['python3', '--version']) 
-                return 'python3'
-            except:
-                print('Python command not found')
-                sys.exit(1)
-
-    python_command = check_python_command()
-
-    is_linux = sys.platform.startswith('linux')
-    is_mac = sys.platform == 'darwin'
-    is_windows = sys.platform in ['win32', 'cygwin']
-
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-
-    if console:
-        os.system(f"{python_command} {script_directory}/console.py {' '.join(args)}")
-    else:
-        if host is None:
-            import socket
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect(("8.8.8.8", 80))
-            host = s.getsockname()[0]
-            s.close()
             
-        load_dotenv()
     
-        username = os.getenv("LODESTONE_USERNAME")
-        password = os.getenv("LODESTONE_PASSWORD")
-        if username is None:
-            print("For security reasons, you must set a username and password to protect your account.")
-            username = input("Username: ")
-            password = input("Password: ")
-            os.environ["LODESTONE_USERNAME"] = f"{username}"
-            os.environ["LODESTONE_PASSWORD"] = f"{password}"
-            with open(".env", "w") as f:
-                f.write(f"LODESTONE_USERNAME={username}\nLODESTONE_PASSWORD={password}")
-                print("Login details saved to .env file. You can change these at any time.")
-        
-        if not os.path.isfile("favicon.ico"):
-            img_data = requests.get("https://github.com/the-lodestone-project/Lodestone/raw/main/assets/favicon.png").content
-            with open('favicon.png', 'wb') as handler:
-                handler.write(img_data)
-        
-        
-        try:
-            lodestone.ui.queue().launch(server_name=f"{host}", server_port=port, show_api=False, auth=(f'{os.environ["LODESTONE_USERNAME"]}', f'{os.environ["LODESTONE_PASSWORD"]}'), share=False, quiet=True, favicon_path="favicon.png", auth_message="Please login with your set username and password. These are not your Minecraft credentials.")
-        except OSError:
-            raise OSError(f"Port {port} is already in use!")
+    
 
 if __name__ == "__main__":
     run()
