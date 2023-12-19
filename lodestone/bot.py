3c452951e2a76659962ad0f3840a018fa3367f88
Silke pilon <silkepilon2009@gmail.com>
2023-12-19 19:35:20 +0100

    fixed viewer bug

--- lodestone/bot.py
+++ lodestone/bot.py
@@ -367,7 +367,9 @@ class Bot:
 
         if not self.skip_checks:
             os.system(f"{sys.executable} -m javascript --install mineflayer")
-            os.system(f"{sys.executable} -m javascript --install prismarine-viewer")
+            
+            # There seems to be a bug with installing prismarine-viewer
+            # os.system(f"{sys.executable} -m javascript --install prismarine-viewer")
             os.system(f"{sys.executable} -m javascript --install mineflayer-collectblock")
             os.system(f"{sys.executable} -m javascript --install minecraft-data")
             os.system(f"{sys.executable} -m javascript --install mineflayer-pathfinder")
@@ -381,7 +383,7 @@ class Bot:
         self.mineflayer = require('mineflayer')
         self.once_with_cleanup = require('mineflayer').promise_utils
         if not self.disable_viewer:
-            self.mineflayer_viewer = require('prismarine-viewer')
+            self.mineflayer_viewer = require('prismarine-viewer', '1.25.0')
         self.python_command = self.__check_python_command()
         if not ls_skip_checks:
             with self.console.status("[bold]Checking for updates...\n") as status:
@@ -509,8 +511,10 @@ class Bot:
     
     def __create_bot(self):
         
-        if not self.local_version in ["1.8.8", "1.9" "15w40b", "1.9.1-pre2", "1.9.2", "1.9.4", "1.10", "16w20a", "1.10-pre1", "1.10", "1.10.1", "1.10.2", "1.11", "16w35a", "1.11", "1.11.2", "1.12", "17w15a", "17w18b", "1.12-pre4", "1.12", "1.12.1", "1.12.2", "1.13", "17w50a", "1.13", "1.13.1", "1.13.2-pre1", "1.13.2-pre2", "1.13.2", "1.14", "1.14", "1.14.1", "1.14.3", "1.14.4", "1.15", "1.15", "1.15.1", "1.15.2", "1.16", "20w13b", "20w14a", "1.16-rc1", "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.17", "21w07a", "1.17", "1.17.1", "1.18", "1.18", "1.18.1", "1.18.2", "1.19", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1", "false"]:
-            raise ValueError(f"Invalid version: {self.local_version}")
+        # TODO: Add a better version check
+        
+        # if not self.local_version in ["1.9" "15w40b", "1.9.1-pre2", "1.9.2", "1.9.4", "1.10", "16w20a", "1.10-pre1", "1.10", "1.10.1", "1.10.2", "1.11", "16w35a", "1.11", "1.11.2", "1.12", "17w15a", "17w18b", "1.12-pre4", "1.12", "1.12.1", "1.12.2", "1.13", "17w50a", "1.13", "1.13.1", "1.13.2-pre1", "1.13.2-pre2", "1.13.2", "1.14", "1.14", "1.14.1", "1.14.3", "1.14.4", "1.15", "1.15", "1.15.1", "1.15.2", "1.16", "20w13b", "20w14a", "1.16-rc1", "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.17", "21w07a", "1.17", "1.17.1", "1.18", "1.18", "1.18.1", "1.18.2", "1.19", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1", "false"]:
+        #     raise ValueError(f"Invalid version: {self.local_version}")
         
         if self.local_version == "auto" or self.local_version == "false":
             self.local_version = False
--- tests/versions.py
+++ tests/versions.py
@@ -1,12 +1,12 @@
 import time
 from rich.console import Console
-from lodestone import createBot
+from bot import createBot
 
 """
 Test different Minecraft/Lodestone versions by creating a bot and checking if it connects successfully.
 """
 
-versions = ["1.8.8", "1.9" "15w40b", "1.9.1-pre2", "1.9.2", "1.9.4", "1.10", "16w20a", "1.10-pre1", "1.10", "1.10.1", "1.10.2", "1.11", "16w35a", "1.11", "1.11.2", "1.12", "17w15a", "17w18b", "1.12-pre4", "1.12", "1.12.1", "1.12.2", "1.13", "17w50a", "1.13", "1.13.1", "1.13.2-pre1", "1.13.2-pre2", "1.13.2", "1.14", "1.14", "1.14.1", "1.14.3", "1.14.4", "1.15", "1.15", "1.15.1", "1.15.2", "1.16", "20w13b", "20w14a", "1.16-rc1", "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.17", "21w07a", "1.17", "1.17.1", "1.18", "1.18", "1.18.1", "1.18.2", "1.19", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1", "false"]
+versions = ["1.9", "1.9.2", "1.9.4", "1.10","1.10", "1.10.1", "1.10.2", "1.11", "1.11", "1.11.2", "1.12", "1.12", "1.12.1", "1.12.2", "1.13", "1.13", "1.13.1", "1.13.2", "1.14", "1.14", "1.14.1", "1.14.3", "1.14.4", "1.15", "1.15", "1.15.1", "1.15.2", "1.16", "1.16", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.17", "1.17", "1.17.1", "1.18", "1.18", "1.18.1", "1.18.2", "1.19", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20", "1.20.1", "false"]
 
 console = Console()
 
@@ -18,14 +18,14 @@ with console.status("[bold]Checking versions...\n") as status:
         # Create bot with the current version  
         try:
             bot = createBot(
-                host='2b2t.org',
+                host='5b5t.org',
                 username='TestBot',  
                 version=version,
                 checkTimeoutInterval=10000,
                 ls_skip_checks=True,
                 hideErrors=False,
                 ls_debug_mode=True,
-                ls_disable_viewer=True
+                ls_disable_viewer=False
             )
         except Exception as err:
             print(err)
